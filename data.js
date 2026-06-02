52471772483890b388f9ba780de2fc620de1d59d
corgiii-w7 <corgiii.w@gmail.com>
2026-06-02 22:04:42 +0900

    refactor: migrate site data from index.html to external data.js module

---
+++ data.js
@@ -0,0 +1,105 @@
+const siteData = {
+  "header": {
+    "catchphrase": "〜いま、ここ、こそ、パラダイス〜"
+  },
+  "about": {
+    "title": "劇団ハレトケとは",
+    "paragraphs": [
+      "私たちは社会人になってもミュージカルに関わりたい！というメンバーが集まり、関東中心に活動をしているミュージカル集団です！",
+      "普段の日常もミュージカルの力でパラダイスにしていこうという心意気で、楽しく活動しております！",
+      "作品は全て団員のオリジナルで、脚本、作詞作曲や、音響・照明・小道具・衣装などスタッフ業も自分たちで試行錯誤しながら担当しております。"
+    ]
+  },
+  "members": {
+    "title": "団員紹介",
+    "description": "キャスト・スタッフの紹介です。",
+    "list": [
+      { "name": "〇〇 〇〇", "role": "団長" },
+      { "name": "△△ △△", "role": "団長" }
+    ]
+  },
+  "stage": {
+    "title": "STAGE",
+    "subtitle": "秋公演企画中！",
+    "description": "次回公演に向けて鋭意稽古中です！\nティザー動画もぜひご覧ください。",
+    "details": [
+      "日程: 2026年10月24日(土) ※予備日: 25日(日)",
+      "会場: 〇〇劇場",
+      "チケット: 準備中"
+    ]
+  },
+  "memory": {
+    "title": "MEMORY",
+    "performances": [
+      {
+        "id": "perf6",
+        "title": "第5回公演「怪盗スターダスト」",
+        "description": "あらすじや、公演に込めた思いなどを記載します。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "Stardust_mv.JPG"
+      },
+      {
+        "id": "perf5",
+        "title": "第4回公演「Terreur」",
+        "description": "あらすじや、公演に込めた思いなどを記載します。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "Terreur.JPG"
+      },
+      {
+        "id": "perf4",
+        "title": "グリーンカレッジ公演「注文の多い音楽会」",
+        "description": "あらすじや、公演に込めた思いなどを記載します。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "Chumonnoooi.PNG"
+      },
+      {
+        "id": "perf3",
+        "title": "第3回公演「オールド・サマー・バケーション」",
+        "description": "あらすじや、公演に込めた思いなどを記載します。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "OSV_mv_0814.jpg"
+      },
+      {
+        "id": "perf2",
+        "title": "第2回公演「ゴースト・アクター」",
+        "description": "あらすじや、公演に込めた思いなどを記載します。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "GA_mv_A3_0109_big.jpg"
+      },
+      {
+        "id": "perf1",
+        "title": "旗揚げ公演「ハレ舞台」",
+        "description": "劇団ハレトケの記念すべき第一回公演。",
+        "date": "202X年 X月",
+        "venue": "〇〇劇場",
+        "image": "harebutai_MV_for-SNS.jpg"
+      }
+    ]
+  },
+  "links": {
+    "title": "団員募集について",
+    "recruitment": [
+      "🎭✨新メンバー大募集✨🎭<br>歌って！踊って！演じてみたいそこのあなた！<br>私たちと一緒にミュージカルをはじめてみませんか？",
+      "＼こんな人、大歓迎！／<br>🎤歌うのが好き！<br>💃ダンスが楽しい！<br>🎭お芝居やってみたい！<br>🎨小道具・衣装づくりが好き！<br>🎼曲を作ってみたい！<br>📝脚本書いてみたい！<br>舞台の上も、裏方も、みんな主役🌟<br>初心者・未経験でもOK！"
+    ],
+    "fee": "【会費】<br>団体費:上期下期各1,000円<br>別途公演参加費:10,000円程度<br>※公演参加は必須ではありません<br>公演に役者として関わる場合にのみ発生",
+    "linkText": "「劇団員になろうよ」募集ページ",
+    "linkUrl": "https://theatrical.net-menber.com/look/data/216338.html?utm_source=ig&utm_medium=social&utm_content=link_in_bio&fbclid=PAdGRleARheVBleHRuA2FlbQIxMQBzcnRjBmFwcF9pZA8xMjQwMjQ1NzQyODc0MTQAAadUoPp6z35Lg9kmNLe7wWCzOyfvG808flBKRdYHTxTvCdojMudfaVHRfsGx2g_aem_yfhOUztngHnuBaVnc2Kjn"
+  },
+  "contact": {
+    "title": "お問い合わせ / 各種SNS",
+    "description1": "公演に関するご質問や、チケットのお問い合わせは以下のフォームより承っております。",
+    "formText": "お問い合わせフォームはこちら",
+    "formUrl": "https://docs.google.com/forms/d/e/1FAIpQLSelOIThFwEVI8Yc99L6PChKt-mCClAt8G852VLiGTJseDqEqg/viewform?usp=header",
+    "snsTitle": "各種SNS",
+    "twitterId": "@haretokemusical",
+    "twitterUrl": "https://x.com/haretokemusical?s=21&t=mtbf5MTQ_Et1zKMI2koXbw",
+    "instagramId": "@haretoke.musical",
+    "instagramUrl": "https://www.instagram.com/haretoke.musical?igsh=bDhucXAyNXgzcXB3&utm_source=qr"
+  }
+};
